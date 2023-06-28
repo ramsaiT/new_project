899855de8b2631e7fb1964a80166de35457c7aa3
ramsaiT <62746903+ramsaiT@users.noreply.github.com>
2023-06-28 13:39:20 +0530

    Update test_pipeline.py

--- Functions/test_pipeline.py
+++ Functions/test_pipeline.py
@@ -99,7 +99,8 @@ def pipeline(master_conf, cleaned_data, output_name, output_format):
                                                                                                        "-").replace(
         ":", "_")
 
-	
+
+    print(current_datetime)
     # examples to save data into Data Inputs
     cleaned_data = cleaned_data.iloc[:100]
     cleaned_data.to_csv(os.path.join(output_path, output_name + output_format), index=False)
